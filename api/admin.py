4648fd06a0c565bf7683d287147091353f5d3db3
geniusrks1 <gmrohitkumar@gmail.com>
2023-12-08 08:36:25 +0530

    authenticatin part done now only authenticated login user can perform curd

--- api/admin.py
+++ api/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
 
 # Register your models here.
+
+
+from api.models import Task
+# Register your models here..
+
+# class TaskAdmin(admin.ModelAdmin):
+#     list_display=( "title","description","due_date","status" )
+#     search_fields=('title',)   
+     
+
+admin.site.register(Task)
+
